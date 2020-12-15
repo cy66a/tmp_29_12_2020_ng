fe1cb7ef69bb7234a21ea0c2055d38455302397f
sasd <jayceon310893@gmail.com>
2020-12-15 21:39:51 +0300

    Update script.js

--- Subbotin/script.js
+++ Subbotin/script.js
@@ -3,9 +3,10 @@ const field = [
   [null, null, null],
   [null, null, null],
 ]
+let wrapper = document.querySelector('.wrapper');
 
+function render () {
 let str = '';
-
 field.forEach((row, x) => {
   str += '<div class="field__row">';
   row.forEach((cell, y) => {
@@ -13,26 +14,28 @@ field.forEach((row, x) => {
   });
   str += '</div>';
 });
-
-let wrapper = document.querySelector('.wrapper');
 wrapper.innerHTML = str;
+}
+
+render();
+
 wrapper.addEventListener('click', (event) => {
   let element = event.target;
   let isCellClick = element.classList.contains('field__cell');
   if (!isCellClick) {
     return;
   }
-  console.log('x:' + element.dataset.x + ', y: ' + element.dataset.y)
-  setCell();
+  // console.log('x:' + element.dataset.x + ', y: ' + element.dataset.y)
+  setCell(element.dataset.y, element.dataset.x, 'value');
+  console.log(field);
+  render();
 })
 
 
 function setCell(y, x, value) {
-  field[x][y] = value;
-
+  field[y][x] = value;
 }
 
-setCell(0, 0, 'Ноль');
 
 
 // console.log(field);
